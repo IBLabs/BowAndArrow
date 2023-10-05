34179fb8bffdb6b82538e134eef9d3d8eec66507
lasrysha <lasrysha@post.bgu.ac.il>
2023-10-05 13:17:41 +0300

    ballon still nor recgonize colision

---
+++ Assets/BowAndArrowImproved/Scripts/Ballon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+public class Ballon : MonoBehaviour
+{
+    [SerializeField] private float buoyancyForce = 10f;
+    [SerializeField] private UnityEvent<GameObject> onPop;
+    [SerializeField] private List<AudioClip> popClips;
+    [SerializeField] private Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        Vector3 buoyancyDirection = Vector3.up * buoyancyForce;
+        rb.AddForce(buoyancyDirection, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("hitttttt");
+        gameObject.SetActive(false);
+        
+        AudioSource.PlayClipAtPoint(popClips[Random.Range(0, popClips.Count)], transform.position);
+    }
+}
---
+++ Assets/BowAndArrowImproved/Scripts/BallonsSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BallonsSpawner : MonoBehaviour
+{
+    [SerializeField] private List<Transform> spawnLocations;
+    [SerializeField] private ParticleSystem.MinMaxCurve spawnInterval;
+    [SerializeField] private Ballon ballonPrefab;
+    [SerializeField]private int numOfBallons;
+    
+    private List<Ballon> _spawnedBallons = new List<Ballon>();
+
+    private float _spawnTimer;
+
+
+    private void Update()
+    {
+        GenerateBallons();
+    }
+
+    private void GenerateBallons()
+    {
+        if (numOfBallons <= 0) return;
+        
+        if (_spawnTimer <= 0)
+        {
+            Transform spawnLocation = spawnLocations[Random.Range(0, spawnLocations.Count)];
+            Ballon newBallon = Instantiate(ballonPrefab, spawnLocation.position, Quaternion.identity);
+            _spawnedBallons.Add(newBallon);
+
+            numOfBallons--;
+    
+            _spawnTimer = spawnInterval.Evaluate(Random.value);
+        }
+        else
+        {
+            _spawnTimer -= Time.deltaTime;
+        }
+    }
+}
