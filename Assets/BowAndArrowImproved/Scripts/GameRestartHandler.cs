3c61b29650a601055127c0bc78ddbea54ae24924
lasrysha <lasrysha@post.bgu.ac.il>
2023-10-29 13:09:52 +0200

    seperate restart and wave handler

---
+++ Assets/BowAndArrowImproved/Scripts/GameRestartHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameRestartHandler : MonoBehaviour
+{
+    [SerializeField] private Ballon restartBalloonPrefab;
+    
+    public void OnGameManagerStateChanged(GameManager.State newState)
+    {
+        if (newState == GameManager.State.Lose)
+        {
+            ShowShotToStartObject();
+        }
+    }   
+
+    public void ShowShotToStartObject()
+    {
+        Ballon newBalloon = Instantiate(restartBalloonPrefab, transform.position, Quaternion.identity);
+        newBalloon.isBalloonFrozen = true;
+    }
+}
--- Assets/BowAndArrowImproved/Scripts/WaveStartHandler.cs
+++ Assets/BowAndArrowImproved/Scripts/WaveStartHandler.cs
@@ -4,44 +4,26 @@ using UnityEngine.Events;
 public class WaveStartHandler : MonoBehaviour
 {
     [SerializeField] private Ballon startBalloonPrefab;
-    [SerializeField] private Ballon restartBalloonPrefab;
     
     public UnityEvent startWave;
-    public UnityEvent resetGame;
-
-    private bool _isStartBalloon;
-
+    
     public void OnGameManagerStateChanged(GameManager.State newState)
     {
         if (newState == GameManager.State.PreWave)
         {
-            _isStartBalloon = true;
-            ShowShotToStartObject(startBalloonPrefab);
+            ShowShotToStartObject();
         } 
-        else if (newState == GameManager.State.Lose)
-        {
-            _isStartBalloon = false;
-            ShowShotToStartObject(restartBalloonPrefab);
-        }
     }   
 
-    public void ShowShotToStartObject(Ballon shotToStartPrefab)
+    public void ShowShotToStartObject()
     {
-        Ballon newBalloon = Instantiate(shotToStartPrefab, transform.position, Quaternion.identity);
+        Ballon newBalloon = Instantiate(startBalloonPrefab, transform.position, Quaternion.identity);
         newBalloon.isBalloonFrozen = true;
         newBalloon.onDeath.AddListener(OnShotToStartObjectPopped);
     }
 
     public void OnShotToStartObjectPopped(GameObject died)
     {
-        if (_isStartBalloon)
-        {
-            startWave?.Invoke();
-        }
-        else
-        {
-            resetGame?.Invoke();
-        }
+        startWave?.Invoke();
     }
-    
 }
