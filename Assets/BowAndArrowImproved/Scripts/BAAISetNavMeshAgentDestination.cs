09180ac975d2cda04e19a7e93c4e77aabb3e6abc
lasrysha <lasrysha@post.bgu.ac.il>
2023-10-30 21:32:26 +0200

    reset BAAISetNavMeshAgentDestination back to main to prevent undefined behaviors

--- Assets/BowAndArrowImproved/Scripts/BAAISetNavMeshAgentDestination.cs
+++ Assets/BowAndArrowImproved/Scripts/BAAISetNavMeshAgentDestination.cs
@@ -7,8 +7,13 @@ public class BAAISetNavMeshAgentDestination : MonoBehaviour, BAAIINavMeshAgentHo
 
     [HideInInspector] public Transform target;
 
+    void Start()
+    {
+        agent.destination = target.position;
+    }
+
     public void SetTargetTransform(Transform target)
     {
-        agent.SetDestination(this.target.position);
+        this.target = target;
     }
 }
