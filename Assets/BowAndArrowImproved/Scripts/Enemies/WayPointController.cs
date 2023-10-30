735874b9034e1453bf59eed6e1b85895ee3ddd63
lasrysha <lasrysha@post.bgu.ac.il>
2023-10-30 21:27:53 +0200

    work with layer mask to detect tags. add err for empty waypoints

--- Assets/BowAndArrowImproved/Scripts/Enemies/WayPointController.cs
+++ Assets/BowAndArrowImproved/Scripts/Enemies/WayPointController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -6,12 +7,17 @@ using Random = UnityEngine.Random;
 public class WayPointController : MonoBehaviour
 {
     [SerializeField] private Transform[] wayPoints;
-    [SerializeField] private string enemyTag = "Enemy";
-
+    [SerializeField] private LayerMask hitMask;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag(enemyTag) || wayPoints.Length == 0) return;
+        if (!hitMask.Contains(other.attachedRigidbody.gameObject.layer)) return;
 
+        if (wayPoints.Length == 0)
+        {
+            throw new Exception($"[ERROR]: wayPoints list is empty in : {gameObject.name}");
+        }
+        
         if (other.attachedRigidbody.TryGetComponent(out NavMeshAgent agent))
         {
             Transform newWayPoint = wayPoints[Random.Range(0, wayPoints.Length)];
