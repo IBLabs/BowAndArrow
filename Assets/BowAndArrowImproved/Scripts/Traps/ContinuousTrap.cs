f2fce44e1aefdafc66df6711b7ff1b42e94c279a
lasrysha <lasrysha@post.bgu.ac.il>
2023-11-02 13:08:34 +0200

    spikes tap mehcanism is finished. to do - activation trap hadle object

---
+++ Assets/BowAndArrowImproved/Scripts/Traps/ContinuousTrap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class ContinuousTrap : MonoBehaviour
+{
+    [SerializeField] private bool isActive;
+    [SerializeField] private Animator animator;
+    [SerializeField] private String activateTrigger;
+    [SerializeField] private String deactivateTrigger;
+    
+    public void ToggleTrapActivation(bool didActivated)
+    {
+        isActive = didActivated;
+
+        animator.SetTrigger(isActive ? activateTrigger : deactivateTrigger);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!isActive) return;
+        if (other.attachedRigidbody.TryGetComponent(out BAAIEnemy hitEnemy)) hitEnemy.Die(true);
+    }
+}
---
+++ Assets/BowAndArrowImproved/Scripts/Traps/TrapActivation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class TrapActivation : MonoBehaviour
+{
+    [SerializeField] private float activationTime = 3f;
+    [SerializeField] private ContinuousTrap myTrap;
+    [SerializeField] private LayerMask hitMask;
+    [SerializeField] private bool isTrapActive;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hitMask.Contains(other.attachedRigidbody.gameObject.layer) || isTrapActive) return;
+        
+        isTrapActive = true;
+        myTrap.ToggleTrapActivation(true);
+
+        StartCoroutine(CountdownAndDeactivateTrap());
+    }
+    
+    private IEnumerator CountdownAndDeactivateTrap()
+    {
+        yield return new WaitForSeconds(activationTime);
+
+        myTrap.ToggleTrapActivation(false);
+        isTrapActive = false;
+    }
+}
