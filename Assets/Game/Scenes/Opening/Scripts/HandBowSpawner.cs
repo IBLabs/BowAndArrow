2d50830967f321304721cbd82a8f057a54657e36
Itamar Biton <45729602+itamarbiton@users.noreply.github.com>
2023-12-07 13:12:40 +0200

    updated bow to be able to held by both hands

--- Assets/Game/Scenes/Opening/Scripts/HandBowSpawner.cs
+++ Assets/Game/Scenes/Opening/Scripts/HandBowSpawner.cs
@@ -1,45 +1,119 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
-using XRController = UnityEngine.InputSystem.XR.XRController;
 
 public class HandBowSpawner : MonoBehaviour
 {
-    [SerializeField] private XRGrabInteractable _interactable;
+    [SerializeField] private XRGrabInteractable bowInteractable;
+    [SerializeField] private XRGrabInteractable stringInteractable;
 
     [SerializeField] private AudioClip onActionSoundEffect;
-    
-    private ActionBasedController _controller;
-    private XRBaseInteractor _interactor;
+
+    [SerializeField] private ActionBasedController leftHandController;
+    [SerializeField] private ActionBasedController rightHandController;
+
+    [SerializeField] private XRBaseInteractor leftHandInteractor;
+    [SerializeField] private XRBaseInteractor rightHandInteractor;
+
     private AudioSource _audioSource;
-    
+    private bool _isBowGrabbed = false;
+    private bool _isBowLeftHand = false;
+    private bool _isStringGrabbed = false;
+
     void Start()
     {
-        _controller = GetComponent<ActionBasedController>();
-        _controller.activateAction.action.started += OnActivateStarted;
-        _controller.activateAction.action.canceled += OnActivateCancelled;
+        leftHandController.activateAction.action.started += OnLeftActivateStarted;
+        leftHandController.activateAction.action.canceled += OnLeftActivateCancelled;
 
-        _interactor = GetComponent<XRBaseInteractor>();
+        rightHandController.activateAction.action.started += OnRightActivateStarted;
+        rightHandController.activateAction.action.canceled += OnRightActivateCancelled;
 
         _audioSource = GetComponent<AudioSource>();
     }
 
-    private void OnActivateStarted(InputAction.CallbackContext context)
+    private void OnLeftActivateStarted(InputAction.CallbackContext context)
     {
-        _interactor.StartManualInteraction(_interactable as IXRSelectInteractable);
-
-        if (_audioSource != null && onActionSoundEffect != null)
-        {
-            _audioSource.PlayOneShot(onActionSoundEffect);
-        }
-
-        _controller.SendHapticImpulse(0.1f, 0.1f);
+        GrabBowOrString(leftHandInteractor, leftHandController, true);
     }
 
-    private void OnActivateCancelled(InputAction.CallbackContext context)
+    private void OnRightActivateStarted(InputAction.CallbackContext context)
     {
-        _interactor.EndManualInteraction();
+        GrabBowOrString(rightHandInteractor, rightHandController, false);
+    }
+
+    private void GrabBowOrString(XRBaseInteractor interactor, XRBaseController controller, bool isLeft)
+    {
+        if (!_isBowGrabbed)
+        {
+            GrabBow(interactor, controller);
+            _isBowLeftHand = isLeft;
+        }
+        else
+        {
+            GrabString(interactor);
+        }
+    }
+
+    private void OnLeftActivateCancelled(InputAction.CallbackContext context)
+    {
+        ReleaseGrabbedItem(leftHandInteractor);
+
+        if (_isBowGrabbed && _isBowLeftHand)
+        {
+            ReleaseGrabbedItem(rightHandInteractor);
+        }
+    }
+
+    private void OnRightActivateCancelled(InputAction.CallbackContext context)
+    {
+        ReleaseGrabbedItem(rightHandInteractor);
+
+        if (_isBowGrabbed && !_isBowLeftHand)
+        {
+            ReleaseGrabbedItem(leftHandInteractor);
+        }
+    }
+
+    private void GrabBow(XRBaseInteractor interactor, XRBaseController controller)
+    {
+        interactor.StartManualInteraction(bowInteractable as IXRSelectInteractable);
+
+        _isBowGrabbed = true;
+
+        if (onActionSoundEffect != null)
+        {
+            AudioSource.PlayClipAtPoint(onActionSoundEffect, transform.position);
+        }
+
+        controller.SendHapticImpulse(0.1f, 0.1f);
+    }
+
+    private void GrabString(XRBaseInteractor interactor)
+    {
+        if (interactor.isPerformingManualInteraction)
+        {
+            Debug.Log("[WARNING]: interactor is already performing manual interaction!");
+            return;
+        }
+
+        interactor.StartManualInteraction(stringInteractable as IXRSelectInteractable);
+
+        _isStringGrabbed = true;
+    }
+
+    private void ReleaseGrabbedItem(XRBaseInteractor interactor)
+    {
+        if (!interactor.isPerformingManualInteraction)
+        {
+            Debug.Log("[WARNING]: interactor is not performing manual interaction but was asked to release!");
+            return;
+        }
+
+        if (_isBowGrabbed && interactor == bowInteractable.GetOldestInteractorSelecting())
+        {
+            _isBowGrabbed = false;
+        }
+
+        interactor.EndManualInteraction();
     }
 }
--- Assets/Game/Scenes/Opening/Scripts/OpeningSceneBowController.cs
+++ Assets/Game/Scenes/Opening/Scripts/OpeningSceneBowController.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
-using DG.Tweening.Core;
-using DG.Tweening.Plugins.Options;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
