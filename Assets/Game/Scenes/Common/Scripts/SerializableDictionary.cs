6cd9440c6fbf8248a85c8ecc24233a8c276f5851
lasrysha <lasrysha@post.bgu.ac.il>
2024-01-18 20:40:50 +0200

    replaced arrow dictionary with generic seriliazble dictionary

---
+++ Assets/Game/Scenes/Common/Scripts/SerializableDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializableDictionary<TKey, TValue>
+{
+    [SerializeField] private List<SerializableDictionaryEntry<TKey, TValue>> entries;
+    [SerializeField] private TValue defaultValue;
+
+    public TValue GetByKey(TKey key)
+    {
+        foreach (var entry in entries)
+        {
+            if (EqualityComparer<TKey>.Default.Equals(entry.Key, key))
+            {
+                return entry.Value;
+            }
+        }
+
+        return defaultValue;
+    }
+}
--- Assets/Game/Scripts/ArrowConfigurationManager.cs
+++ Assets/Game/Scripts/ArrowConfigurationManager.cs
@@ -1,11 +1,10 @@
 ﻿using Game.Scenes.Common.ScriptableObjects;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class ArrowConfigurationManager : MonoBehaviour
 {
     public ArrowConfiguration arrowConfig;
-    [SerializeField] private ArrowDataDictionary arrowsConfigurations;
+    [SerializeField] private SerializableDictionary<ArrowType, ArrowConfiguration> arrowsConfigurations;
 
     public void OnBalloonDeath(GameObject balloonGameObject, int score, bool killedByPlayer)
     {
@@ -15,14 +14,6 @@ public class ArrowConfigurationManager : MonoBehaviour
 
     public void SwitchArrow(ArrowType arrowType)
     {
-        arrowConfig = arrowsConfigurations.GetConfigurationByArrowType(arrowType);
-    }
-
-    public void RandomSwitchArrow()
-    {
-        ArrowType[] arrowTypes = (ArrowType[])System.Enum.GetValues(typeof(ArrowType));
-        ArrowType randomArrowType = arrowTypes[Random.Range(0, arrowTypes.Length)];
-
-        SwitchArrow(randomArrowType);
+        arrowConfig = arrowsConfigurations.GetByKey(arrowType);
     }
 }
